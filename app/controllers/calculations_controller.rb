2a065d72965b68f288ff16035bbb02954883eee8
vperez10 <vperez@chicagobooth.edu>
2015-08-09 19:49:49 -0400

    Fixed variance and std deviation equations
    I had my std dev equation before variance, and was trying to use my
    variance. Furthermore, my variance calculation was all wrong.

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -10,21 +10,21 @@ class CalculationsController < ApplicationController
     @character_count_without_spaces = @text.length-@text.count(" ")
     @word_count = @text.split.count
     @occurrences = @text_lc.split.count(@special_word_lc)
-  end
+end
 
-  def loan_payment
+def loan_payment
     @apr = params[:annual_percentage_rate].to_f
     @years = params[:number_of_years].to_i
     @principal = params[:principal_value].to_f
 
-  n = @apr * 12
+    n = @apr * 12
 
-  rate = @apr/100/12
+    rate = @apr/100/12
 
     @monthly_payment = (rate * @principal)/(1-(1+rate)**(-n))
-  end
+end
 
-  def time_between
+def time_between
     @starting = Chronic.parse(params[:starting_time])
     @ending = Chronic.parse(params[:ending_time])
 
@@ -35,11 +35,11 @@ class CalculationsController < ApplicationController
     @days = @seconds/86400
     @weeks = @seconds/604800
     @years = @seconds/31536000
-  end
+end
 
-  def descriptive_statistics
+def descriptive_statistics
     @numbers = params[:list_of_numbers].gsub(',',' ').split.map(&:to_f)
-     @sorted_numbers = @numbers.sort
+    @sorted_numbers = @numbers.sort
     @count = @numbers.count
     @minimum = @numbers.min
     @maximum = @numbers.max
@@ -47,18 +47,15 @@ class CalculationsController < ApplicationController
     @median = (@minimum + @maximum)/2
     @sum = @numbers.sum
     @mean = @sum/@count
-    @standard_deviation = Math.sqrt(@variance)
     @mode = @numbers.uniq.max_by{ |i| @numbers.count( i ) }
-end
-
-
-def variance(list_of_numbers)
-running_total = 0
-count_for_mean = list_of_numbers.count
-list_of_numbers.each do |number|
-running_total = running_total + (((mean_first_dataset) - number)**2)
-end
-
-
+    squared_differences = []
+    @numbers.each do |num|
+      difference = num - @mean
+      squared_difference = difference ** 2
+      squared_differences.push(squared_difference)
+      @variance = squared_differences.sum/@count
+      @standard_deviation = Math.sqrt(@variance)
   end
+
+end
 end
